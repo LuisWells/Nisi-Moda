b092de5eb5836b8648e2a6ccc6971992f947b2f2
Duberly Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-07-17 05:50:21 -0500

    Update cart UI functionality and minor enhancements
    A variety of changes have been made to the 'carrito.html', 'index.html', 'script.js', and 'WebSiteController.java' to improve the UI functionality and user experience on the shopping cart page. This includes alignment improvements, adding the ability to change item quantities, removing items from the cart and implementing a new payment modal. Additionally, code cleanliness and efficiency were improved by removing console logs and applying changes to array initialisations.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,24 +7,39 @@ window.onload = function cargarCarritoOnLoad() {
     if (cookieExistente !== undefined) {
         //Sí existen items en el carrito, por tanto, actualizamos el indicador
         listaExistente = JSON.parse(cookieExistente);
-        console.log("Se encontraron items en un carrito anterior... actualizando la UI")
-        console.log(listaExistente);
+        // console.log("Se encontraron items en un carrito anterior... actualizando la UI")
+        // console.log(listaExistente);
         itemsEnCarrito = listaExistente.length;
         const labelCarrito = document.getElementById("indicador-carrito");
         labelCarrito.innerText = itemsEnCarrito;
         let precioTotal = 0;
 
         for (const item of listaExistente) {
-            precioTotal = precioTotal + item['precio'];
-            console.log(item)
+            //TODO: actualizar precio del carrito al cambiar cantidades, sin actualizar la página
+            precioTotal = precioTotal + (item.precio * item.cantidad);
+            // actualizamos la lista para no agregar nuevamente lo ya agregado
+            const productoEnCarrito = document.getElementById("agregar-carrito-producto-" + item.productoId);
+            if (productoEnCarrito !== null) {
+                productoEnCarrito.classList.remove("btn-outline-primary");
+                productoEnCarrito.classList.add("btn-outline-success");
+                productoEnCarrito.classList.add("disabled");
+                productoEnCarrito.innerHTML = "<i class='bi bi-check-lg'></i> En Carrito</a>";
+                // productoEnCarrito
+                //     .getElementsByTagName("i")[0]
+                //     .classList.replace("bi-cart-plus", "bi-check");
+            }
+
         }
-        console.log("Precio total:")
-        console.log(precioTotal)
+        // console.log("Precio total:")
+        // console.log(precioTotal)
         const labelCostoTotal = document.getElementById("monto-costo-total");
         if (labelCostoTotal !== null) {
             labelCostoTotal.innerText = "Total: S/. " + String(precioTotal.toFixed(2));
         }
     }
+    //Inicializar los tooltips:
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 }
 
 function agregarListaItemACarrito(productoId, productoPrecio) {
@@ -35,7 +50,7 @@ function agregarListaItemACarrito(productoId, productoPrecio) {
     if (cookieExistente !== undefined) {
         //Sí existen items en el carrito
         nuevaListaItems = JSON.parse(cookieExistente);
-        console.log(nuevaListaItems);
+        // console.log(nuevaListaItems);
     }
     const id = Number(productoId);
     const precio = Number(productoPrecio);
@@ -62,7 +77,7 @@ function agregarDetalleItemACarrito(productoId, productoPrecio) {
     if (cookieExistente !== undefined) {
         //Sí existen items en el carrito
         nuevaListaItems = JSON.parse(cookieExistente);
-        console.log(nuevaListaItems);
+        // console.log(nuevaListaItems);
     }
     const inputCantidad = document.getElementById("cantidad-input");
     const id = Number(productoId);
@@ -88,8 +103,8 @@ function cargarCarrito() {
         // itemsListContainer.innerHTML='';
         const carritoArray = JSON.parse(carrito)
         carritoArray.forEach(item => {
-            console.log("new item!");
-            console.log(item);
+            // console.log("new item!");
+            // console.log(item);
         })
     }
 }
@@ -102,4 +117,64 @@ function eliminarCarrito() {
     Cookies.remove("carritoNiSi");
     console.log("Carrito borrado!");
     window.location.reload();
+}
+
+function eliminarItemCarrito(idProducto) {
+    const id = Number(idProducto);
+    let carrito = Cookies.get("carritoNiSi");
+    if (carrito !== undefined) {
+        const itemsListContainer = document.getElementById("contenedor-lista-items");
+        // itemsListContainer.innerHTML='';
+        const carritoArray = JSON.parse(carrito)
+        let nuevoCarrito = carritoArray.filter((item) => item.productoId !== id);
+        Cookies.set("carritoNiSi", JSON.stringify(nuevoCarrito));
+        window.location.reload();
+    }
+}
+
+//TODO: aumentar o reducir cuando el producto ya existe y se vuelve a agregar al carro, o mejor,
+// deshabilitar el botón de agregar y mostrar qu esta agregado ya
+function aumentarCantidadItem(idProducto) {
+    const id = Number(idProducto);
+    const labelCantidad = document.getElementById("cantidad-producto-" + idProducto);
+    if (labelCantidad !== null) {
+        console.log("aumentando cantidad del producto con ID " + idProducto)
+        let actual = Number(labelCantidad.innerText);
+        if (actual < 100) { //máximo 100 items del mismo producto por venta
+            labelCantidad.innerText = String(actual + 1);
+            let carrito = Cookies.get("carritoNiSi");
+            if (carrito !== undefined) {
+                const carritoArray = JSON.parse(carrito)
+                carritoArray.forEach((item) => {
+                    if (item.productoId === id) {
+                        item.cantidad = actual + 1;
+                    }
+                });
+                Cookies.set("carritoNiSi", JSON.stringify(carritoArray));
+            }
+        }
+
+    }
+}
+
+function reducirCantidadItem(idProducto) {
+    const id = Number(idProducto);
+    const labelCantidad = document.getElementById("cantidad-producto-" + idProducto);
+    if (labelCantidad !== null) {
+        console.log("aumentando cantidad del producto con ID " + idProducto)
+        let actual = Number(labelCantidad.innerText);
+        if (actual >= 2) {
+            labelCantidad.innerText = String(actual - 1);
+            let carrito = Cookies.get("carritoNiSi");
+            if (carrito !== undefined) {
+                const carritoArray = JSON.parse(carrito)
+                carritoArray.forEach((item) => {
+                    if (item.productoId === id) {
+                        item.cantidad = actual - 1;
+                    }
+                });
+                Cookies.set("carritoNiSi", JSON.stringify(carritoArray));
+            }
+        }
+    }
 }
